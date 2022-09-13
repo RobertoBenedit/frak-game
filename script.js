a5ad1ce048302c5d72c1e3976bfce7cb923ee725
RobertoBenedit <roberto.benedit.contador@gmail.com>
2022-09-13 09:52:35 -0300

    player

--- script.js
+++ script.js
@@ -16,7 +16,20 @@ window.addEventListener("load", function () {
         constructor() {}
     }
     class Player {
-        constructor() {}
+        constructor(game) {
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+        }
+        update() {
+            this.y += this.speedY;
+        }
+        draw(context) {
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
     class Enemy {
         constructor() {}
@@ -32,6 +45,14 @@ window.addEventListener("load", function () {
     }
 
     class Game {
-        constructor() {}
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+        }
+        update() {
+            this.player.update()
+        }
+        draw() {}
     }
 });
