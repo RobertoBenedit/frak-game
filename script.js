cf0a7d780b32365b7223262a54f75b26ce31a473
RobertoBenedit <roberto.benedit.contador@gmail.com>
2022-09-13 01:23:03 -0300

    class

---
+++ script.js
@@ -0,0 +1,37 @@
+window.addEventListener("load", function () {
+    const canvas = document.getElementById("canvas1");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 500;
+    canvas.height = 500;
+
+    class InputHandler {
+        constructor() {}
+    }
+
+    class Projectile {
+        constructor() {}
+    }
+
+    class Particle {
+        constructor() {}
+    }
+    class Player {
+        constructor() {}
+    }
+    class Enemy {
+        constructor() {}
+    }
+    class Layer {
+        constructor() {}
+    }
+    class Background {
+        constructor() {}
+    }
+    class UI {
+        constructor() {}
+    }
+
+    class Game {
+        constructor() {}
+    }
+});
